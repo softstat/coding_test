473c372e4f0a18ad78fdf02a89ed562f0eb68bae
김민석 <96642896+softstat@users.noreply.github.com>
2024-07-14 15:30:48 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -0,0 +1,9 @@
+def solution(emergency):
+    answer1 = []
+    answer2 = []
+    
+    answer2 = sorted(emergency,reverse=True)
+    
+    for i in emergency:
+        answer1.append(answer2.index(i)+1)
+    return answer1
