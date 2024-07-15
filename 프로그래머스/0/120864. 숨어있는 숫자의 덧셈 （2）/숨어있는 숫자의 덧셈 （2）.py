ffc933ea619aa3ceaf079c499c29c9965a6ee6bf
김민석 <96642896+softstat@users.noreply.github.com>
2024-07-15 16:50:40 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -10,7 +10,7 @@ def solution(my_string):
                 answer += int(current_number)
                 current_number = ''
     
-    if current_number:  # To add any number left at the end of the string
+    if current_number:
         answer += int(current_number)
     
     return answer
