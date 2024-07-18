c354f7c5bf0930aa5ab08943ef51e85c21d38ce7
김민석 <96642896+softstat@users.noreply.github.com>
2024-07-18 11:27:59 +0900

    [level 0] Title: 가위 바위 보, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -0,0 +1,8 @@
+def solution(rsp):
+    rsp =rsp.replace('2','s')
+    rsp =rsp.replace('5','p')
+    rsp =rsp.replace('0','r')
+    rsp =rsp.replace('r','5')
+    rsp =rsp.replace('s','0')
+    rsp =rsp.replace('p','2')
+    return rsp
