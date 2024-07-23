df4396cbbc062107c7364c5214f093ee02165a9f
김민석 <96642896+softstat@users.noreply.github.com>
2024-07-23 11:18:39 +0900

    [level 0] Title: OX퀴즈, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
@@ -1,20 +1,12 @@
 def solution(quiz):
     answer = []
     for q in quiz:
-        # Split the quiz string into parts
-        parts = q.split()
-        
-        # Extract the left-hand side (lhs) and right-hand side (rhs) of the equation
-        lhs = parts[0] + ' ' + parts[1] + ' ' + parts[2]  # e.g., "3 - 4"
-        rhs = parts[4]  # e.g., "-1"
-        
-        # Evaluate the lhs and compare it with rhs
-        if eval(lhs) == int(rhs):
-            answer.append("O")
+        L,R = q.split("=")
+        a,ot,b = L.split()
+        if ot == "+":
+            result =  "O" if int(a)+int(b) == int(R) else "X"
+            answer.append(result)
         else:
-            answer.append("X")
-            
+            result= "O" if int(a)-int(b) == int(R) else "X"
+            answer.append(result)
     return answer
-
-
-
