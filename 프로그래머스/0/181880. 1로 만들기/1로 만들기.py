028c62cf5f05e961d63c81240ebb9c9ce8fe6d28
김민석 <96642896+softstat@users.noreply.github.com>
2024-07-31 11:44:13 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -1,15 +1,7 @@
 def solution(num_list):
-    total_operations = 0
-    
-    for num in num_list:
-        operations = 0
-        while num != 1:
-            if num % 2 == 0:
-                num //= 2
-            else:
-                num = (num - 1) // 2
-            operations += 1
-        total_operations += operations
-    
-    return total_operations
-
+    answer = 0
+    for i in num_list:
+        while i != 1:
+            i //=2
+            answer += 1
+    return answer
