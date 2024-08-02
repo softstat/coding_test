626af988496cc48b7db8789ff646e3811cabcd02
김민석 <96642896+softstat@users.noreply.github.com>
2024-08-02 11:46:05 +0900

    [level 0] Title: l로 만들기, Time: 2.44 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.py
@@ -0,0 +1,3 @@
+def solution(myString):
+    answer = ''.join(myString.replace("a","l").replace("b","l").replace("c","l").replace("d","l").replace("e","l").replace("f","l").replace("g","l").replace("h","l").replace("i","l").replace("j","l").replace("k","l"))
+    return answer
