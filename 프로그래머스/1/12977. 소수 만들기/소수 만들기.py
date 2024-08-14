6912ffa62aa9e497e04124b8b884fcbf0d762a04
김민석 <96642896+softstat@users.noreply.github.com>
2024-08-14 23:12:18 +0900

    [level 1] Title: 소수 만들기, Time: 18.34 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -0,0 +1,19 @@
+from itertools import combinations
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def solution(nums):
+    answer = 0
+    combs = combinations(nums, 3)
+    
+    for comb in combs:
+        if is_prime(sum(comb)):
+            answer += 1
+
+    return answer
